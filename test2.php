f56c3a9bcc8686102ff2c44ebff413e2de166cb7
ke30305 <walkingfish1110@gmail.com>
2015-01-21 20:41:59 +0800

    commit test

--- test2.php
+++ test2.php
@@ -1,2 +1,3 @@
 <script src='http://wbkuo.twbbs.org/global/js/jquery.js'></script>
-<script src='http://wbkuo.twbbs.org/test.js'></script>
+<script src='http://wbkuo.twbbs.org/test1111.js'></script>
+<script src='http://wbkuo.twbbs.org/test2222.js'></script>
